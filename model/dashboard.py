1dc43ef4f914ba3412decb48003151b3645a6be2
Febryanda <139682985+Ionisasi@users.noreply.github.com>
2025-01-03 22:13:22 +0700

    rework dashboard

--- model/dashboard.py
+++ model/dashboard.py
@@ -1,31 +1,23 @@
 from PySide6.QtWidgets import QMainWindow, QMessageBox
-from view.UI_Dashboard import Ui_Dashboard
+from view.UI_Dashboard import Ui_UI_Dashboard
 
 class Dashboard(QMainWindow):
     def __init__(self):
         super().__init__()
-        self.ui = Ui_Dashboard()
+        self.ui = Ui_UI_Dashboard()
         self.ui.setupUi(self)
 
         # connect buttons
-        self.ui.logout_button.clicked.connect(self.logout)
-        self.ui.kelola_button.clicked.connect(self.toggle_kelola_submenu)
-        self.ui.koleksi_button.clicked.connect(self.toggle_koleksi_submenu)
-
-        # Mengatur eksklusivitas tombol
-        for button in self.ui.sidebar_buttons:
-            button.clicked.connect(lambda checked, btn=button: self.on_sidebar_button_click(btn))
+        self.ui.Logout.clicked.connect(self.logout)
+        self.ui.Data.clicked.connect(self.toggle_kelola_submenu)
+        self.ui.Koleksi.clicked.connect(self.toggle_koleksi_submenu)
     
     def toggle_kelola_submenu(self):
-        self.ui.kelola_submenu.setVisible(not self.ui.kelola_submenu.isVisible())
+        self.ui.DataSubMenu.setVisible(not self.ui.DataSubMenu.isVisible())
 
     def toggle_koleksi_submenu(self):
-        self.ui.koleksi_submenu.setVisible(not self.ui.koleksi_submenu.isVisible())
-
-    def on_sidebar_button_click(self, clicked_button):
-        for button in self.ui.sidebar_buttons:
-            if button != clicked_button:
-                button.setChecked(False)
+        self.ui.KoleksiSubMenu.setVisible(not self.ui.KoleksiSubMenu.isVisible())
+        
     def logout(self):
         # Displaying a message box on logout
         message_box = QMessageBox()
@@ -39,18 +31,3 @@ class Dashboard(QMainWindow):
         from login import Login  # Local import to avoid circular import
         self.login = Login()
         self.login.show()
-        
-    def on_sidebar_button_click(self, clicked_button):
-        for button in self.ui.sidebar_buttons:
-            if button != clicked_button:
-                button.setChecked(False)
-        
-        # Ubah konten utama berdasarkan tombol yang diklik
-        if clicked_button == self.ui.dashboard_button:
-            self.ui.main_content.setText("Dashboard Content")
-        elif clicked_button == self.ui.koleksi_button:
-            self.ui.main_content.setText("Koleksi Content")
-        elif clicked_button == self.ui.kelola_button:
-            self.ui.main_content.setText("Kelola Data Content")
-        elif clicked_button == self.ui.log_data_button:
-            self.ui.main_content.setText("Log Data Content")
